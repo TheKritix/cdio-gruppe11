611cbbed9ccd15e19a7e6019e1152356028251ea
Kristoffer <kristoffer@kritix.dk>
2022-03-16 11:09:53 +0100

    dis is commit of ml

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ImageReg from './components/ImageReg'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ImageReg/>
   </React.StrictMode>,
   document.getElementById('root')
 );

9a77347685c2c6521c09bd97dfccbaf6114a1e56
Camilla <s205360@student.dtu.dk>
2022-03-16 12:08:23 +0100

    change in landingpage

--- src/Landingpage.js
+++ src/Landingpage.js
@@ -1,13 +1,19 @@
 import './Landingpage.css';
 import React from "react";
 import Button from './Components/Button';
+import logo from './assets/images/logo.frontpage.jpg';
 
 
 
 function Landingpage() {
   return (
     <div className="landingpage">
-         <h1 className='title'>Welcome to Solitaire</h1>
+      <img src={logo} className = "frontpage-logo" alt="logo" />
+         <h1 className='title'>Welcome to solve Solitaire</h1>
+         <ul className='button'>
+         <button className='buttonDesign'>One Card</button>
+         <button className='buttonDesign'>Tree Card</button>
+         </ul>
         </div>
   );
 }

3b558928598d4fd17652515f62c52a6422eab607
Camilla <s205360@student.dtu.dk>
2022-03-30 21:47:41 +0200

    pictures deleted and some new modifications

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -5,10 +5,8 @@ import card2 from './card2.png';
 import card3 from './card3.png';
 import card4 from './card4.png';
 import card5 from './card5.png';
+import card6 from './card6.png';
 import card7 from './card7.png';
-import card8 from './card8.png';
-import card9 from './card9.png';
-import card10 from './card10.png';
 
 
 
@@ -19,32 +17,35 @@ function HomePage() {
     Solitaire
      </div>
      <ul className='buttons'>
-     <button className='card1Btn'>1 Card</button>
-     <button className='card3Btn'>3 Cards</button>
+     <button className='oneCardBtn'>1 Card</button>
+     <button className='threeCardBtn'>3 Cards</button>
      </ul>
      
-<div className='emoji iwazaru'>
-<img src={card2}  height="150" ></img>
+<div className='card one'>
+<img src={card1}  height="130" ></img>
  </div>
- <div className='emoji bomb'>
- <img src={card1}  height="100"></img>
+ <div className='card two'>
+ <img src={card2}  height="120"></img>
  </div>
- <div className='emoji fire'>
- <img src={card4} height="200"></img> 
+ <div className='card three'>
+ <img src={card3} height="180"></img> 
  </div>
- <div className='emoji percent'>
- <img src={card8}  height="250"></img>
+ <div className='card four'>
+ <img src={card4}  height="100"></img>
  </div>
- <div className='emoji heart'>
- <img src={card7} height="200"></img>
+ <div className='card five'>
+ <img src={card5} height="130"></img>
  </div>
- 
+ <div className='card six'>
+ <img src={card6} height="180"></img>
+ </div>
+ <div className='card seven'>
+ <img src={card7} height="125"></img>
+ </div>
 
-
+ 
  </div>
 
- 
- 
  
     )
 }

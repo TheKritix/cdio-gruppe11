5da9ff9873e7a5ae01da3c18e7062d6bee3ee733
Camilla <s205360@student.dtu.dk>
2022-03-30 12:16:56 +0200

    added new cards

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import './HomePage.css';
-import card1 from './card1.png'
-import card2 from './card2.png'
-import card8 from './card8.png'
-import card6 from './card6.png'
-import card4 from './card4.png'
-
-
+import card1 from './card1.png';
+import card2 from './card2.png';
+import card3 from './card3.png';
+import card4 from './card4.png';
+import card5 from './card5.png';
+import card7 from './card7.png';
+import card8 from './card8.png';
+import card9 from './card9.png';
+import card10 from './card10.png';
 
 
 
@@ -20,21 +22,25 @@ function HomePage() {
      <button className='card1Btn'>1 Card</button>
      <button className='card3Btn'>3 Cards</button>
      </ul>
+     
 <div className='emoji iwazaru'>
-<img src={card2}  ></img>
+<img src={card2}  height="150" ></img>
  </div>
  <div className='emoji bomb'>
- <img src={card1} className="card1"></img>
+ <img src={card1}  height="100"></img>
  </div>
  <div className='emoji fire'>
- <img src={card4} ></img> 
+ <img src={card4} height="200"></img> 
  </div>
  <div className='emoji percent'>
- <img src={card8} ></img>
+ <img src={card8}  height="250"></img>
  </div>
  <div className='emoji heart'>
- <img src={card6} ></img>
+ <img src={card7} height="200"></img>
  </div>
+ 
+
+
  </div>
 
  

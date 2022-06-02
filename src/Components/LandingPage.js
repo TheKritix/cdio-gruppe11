06a7ebb39a5cea921de44baefdaf65573a63f4fc
Camilla <s205360@student.dtu.dk>
2022-06-02 09:36:43 +0200

    landingpage modified

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import Card1 from '../Assets/Images/card1.png';
-import Card2 from '../Assets/Images/card2.png';
-import Card3 from '../Assets/Images/card3.png';
-import Card4 from '../Assets/Images/card4.png';
-import Card5 from '../Assets/Images/card5.png';
-import Card6 from '../Assets/Images/card6.png';
-import Card7 from '../Assets/Images/card7.png';
-import Card8 from '../Assets/Images/card8.png';
-import Card9 from '../Assets/Images/card9.png';
-import Card10 from '../Assets/Images/card10.png';
+import Card1 from '../assets/Images/card1.png';
+import Card2 from '../assets/Images/card2.png';
+import Card3 from '../assets/Images/card3.png';
+import Card4 from '../assets/Images/card4.png';
+import Card5 from '../assets/Images/card5.png';
+import Card6 from '../assets/Images/card6.png';
+import Card7 from '../assets/Images/card7.png';
+import Card8 from '../assets/Images/card8.png';
+import Card9 from '../assets/Images/card9.png';
+import Card10 from '../assets/Images/card10.png';
 import './LandingPage.css';
 
 

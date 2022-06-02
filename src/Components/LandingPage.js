851523e5905a9e406f4517b376a254641541fcd8
Camilla <s205360@student.dtu.dk>
2022-06-02 11:30:46 +0200

    landingpage modified

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import Card1 from '../assets/Images/card1.png';
 import Card2 from '../assets/Images/card2.png';
@@ -12,21 +11,6 @@ import Card9 from '../assets/Images/card9.png';
 import Card10 from '../assets/Images/card10.png';
 import './LandingPage.css';
 
-=======
-import React, { Component } from "react";
-import Card1 from "../Assets/Images/card1.png";
-import Card2 from "../Assets/Images/card2.png";
-import Card3 from "../Assets/Images/card3.png";
-import Card4 from "../Assets/Images/card4.png";
-import Card5 from "../Assets/Images/card5.png";
-import Card6 from "../Assets/Images/card6.png";
-import Card7 from "../Assets/Images/card7.png";
-import Card8 from "../Assets/Images/card8.png";
-import Card9 from "../Assets/Images/card9.png";
-import Card10 from "../Assets/Images/card10.png";
-import "./LandingPage.css";
-import { Link } from "react-router-dom";
->>>>>>> 203f8dca34f01e23fff8496dd9847527cb738414
 
 class LandingPage extends Component {
   render() {

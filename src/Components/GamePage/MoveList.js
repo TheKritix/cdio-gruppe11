d7c1ddcbfe0159ee6ac0e5e762fddadb7b2e7ed1
lauritzpepke <s191179@student.dtu.dk>
2022-06-10 11:04:09 +0200

    id movelist

--- src/Components/GamePage/MoveList.js
+++ src/Components/GamePage/MoveList.js
@@ -7,19 +7,17 @@ const MoveList = ({moveList}) => {
     console.log(moveList);
 
     const [moves, setMoves] = useState(moveList);
-    
 
     
-
     return (
         <div className="movelist-div">
             <h3 className="movelist-title">
                 List of moves made: 
             </h3>
             <hr className="movelist-divider"/>
-           {moveList.map((move) => (
-               <p className="movelist-text">
-                    {move.desc}
+           {moveList.filter((move) => (move.id > 0)).map((move) => (
+               <p className="movelist-text" key={move.id}>
+                    {move.id}: {move.desc}
                </p>
            ))}
         </div>

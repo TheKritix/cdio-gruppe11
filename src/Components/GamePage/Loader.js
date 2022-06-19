b27b0ab519b8e3f3bc41642a1edf93253f43c999
lauritzpepke <s191179@student.dtu.dk>
2022-06-19 12:40:53 +0200

    new loader

--- src/Components/GamePage/Loader.js
+++ src/Components/GamePage/Loader.js
@@ -1,13 +1,14 @@
 import "../../Pages/GamePage.css"
 import {useRef, useState} from "react"
-import SyncLoader from "react-spinners/SyncLoader";
+import PulseLoader from "react-spinners/PulseLoader";
+
 
 const Loader = ({loading, width, height}) => {
 
     return (
         <div>
             <div className="loader-div" style={{height: height * 0.2, width: width * 0.2}}>
-                <SyncLoader loading={loading} color={"black"}/>
+                <PulseLoader loading={loading} color={"black"}/>
                 <p className="loader-text">Loading model</p>
             </div>
         </div>
